bd396074594ad1310cceb4e8efeef89115df8c34
pinon <ymc9.knit.p23@gmail.com>
2019-05-10 02:09:30 +0900

    DEL→BSPC

--- keyboards/ergo42/keymaps/pinon/keymap.c
+++ keyboards/ergo42/keymaps/pinon/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  @   |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  BS  |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,KC_T,KC_T,          KC_Y,KC_Y,KC_U,    KC_I,     KC_O,     KC_P,     KC_DEL, \
+    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,KC_T,KC_T,          KC_Y,KC_Y,KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_LCTRL,  KC_A,    KC_S,   KC_D,    KC_F,KC_G,KC_G,          KC_H,KC_H,KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,KC_B,KC_B,          KC_N, KC_N,KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_RO), \
     KC_LGUI,  KC_LCTL, KC_GRV, KC_BSLS, KC_LALT, MO(META), KC_SPC, KC_SPC, KC_QUOT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, MO(GAME) \
