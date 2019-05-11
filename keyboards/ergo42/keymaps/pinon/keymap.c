12cdc8d53a367fbe0a54decaed99eddc5ba480d1
pinon <ymc9.knit.p23@gmail.com>
2019-05-11 16:35:08 +0900

    レイヤー名を変更など

--- keyboards/ergo42/keymaps/pinon/keymap.c
+++ keyboards/ergo42/keymaps/pinon/keymap.c
@@ -3,8 +3,8 @@
 extern keymap_config_t keymap_config;
 
 #define BASE 0
-#define META 1
-#define SYMB 2
+#define LOWER 1
+#define RAISE 2
 #define GAME 3
 
 // Fillers to make layering more clear
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |   Z  |   X  |   C  |   V  |   B  |  n   |   |  b   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | LGUI | LCtrl| LGUI | LAlt | META | META |Space |   | enter| SYMB | SYMB |   =  | LEFT | DOWN | RIGHT|
+   * | LGUI | LCtrl| LGUI | LAlt | LOWER | LOWER |Space |   | enter| RAISE | RAISE |   =  | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
    */
 
@@ -29,29 +29,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,KC_T,KC_Y,          KC_T,KC_Y,KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_LCTRL,  KC_A,    KC_S,   KC_D,    KC_F,KC_G,KC_H,          KC_G,KC_H,KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,KC_B,KC_N,          KC_B, KC_N,KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_RO), \
-    XXXXXXX,  XXXXXXX, KC_LGUI, KC_LALT, MO(META), MO(META), KC_SPC, KC_ENT, MO(SYMB), MO(SYMB), KC_EQL, KC_LEFT, KC_DOWN, MO(GAME) \
+    XXXXXXX,  KC_ESC, KC_RGUI, KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_EQL, KC_LEFT, KC_DOWN, MO(GAME) \
   ),
 
-  /* META
+  /* LOWER
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   1  |   2  |   3  |   4  |   5  |   6  |  [   |   |  ]   |   7  |   8  |   9  |   0  |   -  |  ^   |
+   * |      |   1  |   2  |   3  |   4  |   5  |  6   |   |  ]   |   7  |   8  |   9  |   0  |   -  |  ^   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | Alt  |  F1  |  F2  |Muhen | Henk |      |  (   |   |  )   | Left | Down |  Up  |Right |      |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |  F2  |  F3  |  F4  |  F5  |  F6  |  {   |   |  }   |  F7  |  F8  |  F9  | F10  | F11  |\/Sft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |Reset |=>GAME|=>SYMB|  \   |
-   * |      |      |      |      |~SYMB |RCtrl |Shift |   |Space |~META |      |      |      |      |      |
+   * | LGUI | LCtrl| LGUI | LAlt | LOWER | LOWER |Space |   | enter| RAISE | RAISE |   =  | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
    */
-  [META] = LAYOUT( \
-    _______, KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  KC_6, _______, KC_7,    KC_8,    KC_9,  KC_0,    KC_MINS, KC_EQL, \
-    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, _______, _______, KC_LEFT, S(KC_MINS), KC_UP, KC_RGHT, XXXXXXX, XXXXXXX, \
-    _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______, _______, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, _______, _______, _______ \
+  [LOWER] = LAYOUT( \
+    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), _______, S(KC_7), S(KC_8), S(KC_9),    S(KC_0),   S(KC_MINS), S(KC_EQL), \
+    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, _______,    _______, KC_LEFT, KC_MINS, KC_EQL, S(KC_LBRC), S(KC_RBRC), XXXXXXX, \
+    _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,    _______, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
+    _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______ \
   ),
 
-  /* SYMB
+  /* RAISE
    * ,------------------------------------------------.   ,------------------------------------------------.
    * |   !  |   "  |   #  |   $  |   %  |   &  |  [   |   |  ]   |   '  |   (  |   )  |   ~  |   =  |  ~   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
@@ -59,13 +58,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |      |      |      |      |      |  {   |   |  }   |      |      |   <  |   >  |   ?  |  \   |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |PrtSc |=>GAME|=>SYMB|  \   |
-   * |      |      |      |      |~SYMB |RCtrl |Shift |   |Space |~META |      |      |      |      |      |
+   * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |PrtSc |=>GAME|=>RAISE|  \   |
+   * |      |      |      |      |~RAISE |RCtrl |Shift |   |Space |~LOWER |      |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
-  [SYMB] = LAYOUT( \
-    S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), _______, _______, S(KC_7), S(KC_8), S(KC_9),    S(KC_0),   S(KC_MINS), S(KC_EQL), \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, S(KC_7), S(KC_8), KC_MINS,    XXXXXXX,   S(KC_SCLN), S(KC_QUOT), \
+  [RAISE] = LAYOUT( \
+      _______, KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  _______,       _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, S(KC_7),  S(KC_MINS), S(KC_EQL), KC_LBRC,   KC_RBRC, S(KC_QUOT), \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RO), \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,    _______ \
   ),
@@ -78,15 +77,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | Ctrl | GUI  |  App |PrtSc | ESC  |Space |Tab   |   |Back  |Enter | Del  |PrtSc |=>GAME|=>SYMB|  \   |
+   * | Ctrl | GUI  |  App |PrtSc | ESC  |Space |Tab   |   |Back  |Enter | Del  |PrtSc |=>GAME|=>RAISE|  \   |
    * |      |      |      |      |      |      |      |   |Space |      |      |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [GAME] = LAYOUT( \
-    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,   KC_RBRC,    KC_BSLS,    KC_Y,   KC_U,    KC_I,     KC_O,    KC_UP,    KC_LBRC, \
-    KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,   KC_G,   S(KC_8),    S(KC_9),    KC_H,   KC_J,    KC_K,     KC_UP,    KC_RGHT, KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_M,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
-    KC_LCTRL, KC_LGUI, KC_APP, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_PSCR,  _______, _______, _______ \
+    _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,_______, _______,_______,    KC_DEL, \
+    _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,_______, KC_UP, KC_RGHT, KC_QUOT, \
+    _______,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_M,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
+    _______, _______, _______, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_PSCR,  _______, _______, _______ \
   )
+  /*
+   * KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,   KC_RBRC,    KC_BSLS,    KC_Y,   KC_U,    KC_I,     KC_O,    KC_UP,    KC_LBRC, \
+   * KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,   KC_G,   S(KC_8),    S(KC_9),    KC_H,   KC_J,    KC_K,     KC_UP,    KC_RGHT, KC_QUOT, \
+   * KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_M,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
+   * KC_LCTRL, KC_LGUI, KC_APP, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_PSCR,  _______, _______, _______ \
+   */
 
 };
