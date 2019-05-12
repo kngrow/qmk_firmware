55fb4a09fe332bccec5774f9320963d9086663fd
pinon <ymc9.knit.p23@gmail.com>
2019-05-12 17:55:08 +0900

    矢印の仕組みを変更

--- keyboards/ergo42/keymaps/pinon/keymap.c
+++ keyboards/ergo42/keymaps/pinon/keymap.c
@@ -11,6 +11,7 @@ extern keymap_config_t keymap_config;
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
@@ -28,8 +29,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( \
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,KC_T,KC_Y,          KC_T,KC_Y,KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_LCTRL,  KC_A,    KC_S,   KC_D,    KC_F,KC_G,KC_H,          KC_G,KC_H,KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,KC_B,KC_N,          KC_B, KC_N,KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_RO), \
-    XXXXXXX,  KC_ESC, KC_RGUI, KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_EQL, KC_LEFT, KC_DOWN, MO(GAME) \
+    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,KC_B,KC_N,          KC_B, KC_N,KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_ENT), \
+    XXXXXXX,  KC_ESC, KC_RGUI, KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_LEFT, KC_DOWN, KC_DOWN, MO(GAME) \
   ),
 
   /* LOWER
@@ -64,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [RAISE] = LAYOUT( \
     _______, KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  _______,       _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, S(KC_7),  S(KC_MINS), S(KC_EQL), KC_LBRC,   KC_RBRC, S(KC_QUOT), \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, S(KC_7), KC_MINS    , S(KC_EQL), KC_LBRC,   KC_RBRC, S(KC_QUOT), \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RO), \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,    _______ \
   ),
@@ -82,10 +83,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [GAME] = LAYOUT( \
-    _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,_______, _______,_______,    KC_DEL, \
+    _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,KC_PSCR, KC_HOME,_______,    KC_DEL, \
     _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,_______, KC_UP, KC_RGHT, KC_QUOT, \
-    _______,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_M,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
-    _______, _______, _______, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_PSCR,  _______, _______, _______ \
+    _______,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_END,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
+    _______, _______, _______, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_LEFT,  KC_DOWN, _______, _______ \
   )
   /*
    * KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,   KC_RBRC,    KC_BSLS,    KC_Y,   KC_U,    KC_I,     KC_O,    KC_UP,    KC_LBRC, \
