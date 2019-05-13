a981b4eaddf12c79c49fb62225a722816aead333
pinon <ymc9.knit.p23@gmail.com>
2019-05-14 00:21:44 +0900

    GAMEレイヤに設置

--- keyboards/ergo42/keymaps/pinon/keymap.c
+++ keyboards/ergo42/keymaps/pinon/keymap.c
@@ -30,12 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,KC_T,KC_Y,          KC_T,KC_Y,KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_LCTRL,  KC_A,    KC_S,   KC_D,    KC_F,KC_G,KC_H,          KC_G,KC_H,KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,KC_B,KC_N,          KC_B, KC_N,KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_ENT), \
-    XXXXXXX,  KC_ESC, KC_RGUI, KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_LEFT, KC_DOWN, KC_DOWN, MO(GAME) \
+    KC_LCTRL,  KC_ESC, KC_LGUI, KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_LEFT, KC_DOWN, KC_DOWN, MO(GAME) \
   ),
 
   /* LOWER
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |      |   1  |   2  |   3  |   4  |   5  |  6   |   |  ]   |   7  |   8  |   9  |   0  |   -  |  ^   |
+   * |      |   1  |   2  |   3  |   4  |   5  |  6   |   |  ]   |   6  |   8  |     |   0  |   -  |  ^   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | Alt  |  F1  |  F2  |Muhen | Henk |      |  (   |   |  )   | Left | Down |  Up  |Right |      |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [LOWER] = LAYOUT( \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), _______, S(KC_7), S(KC_8), S(KC_9),    S(KC_0),   S(KC_MINS), S(KC_EQL), \
-    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, _______,    _______, KC_LEFT, KC_MINS, KC_EQL, S(KC_LBRC), S(KC_RBRC), XXXXXXX, \
+    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______, _______,S(KC_6), S(KC_7), S(KC_8), S(KC_9),    S(KC_0),  S(KC_EQL), \
+    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_LBRC,    KC_RBRC, KC_LEFT, KC_MINS, KC_EQL, S(KC_LBRC), S(KC_RBRC), KC_BSLS, \
     _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,    _______, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
     _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______ \
   ),
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [RAISE] = LAYOUT( \
     _______, KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  _______,       _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, S(KC_7), KC_MINS    , S(KC_EQL), KC_LBRC,   KC_RBRC, S(KC_QUOT), \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RO), \
+    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_LBRC,    KC_RBRC, S(KC_7), KC_MINS    , S(KC_EQL), KC_LBRC,   KC_RBRC, S(KC_BSLS), \
+    _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,    _______, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,    _______ \
   ),
 
@@ -84,8 +84,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [GAME] = LAYOUT( \
     _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,KC_PSCR, KC_HOME,_______,    KC_DEL, \
-    _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,_______, KC_UP, KC_RGHT, KC_QUOT, \
-    _______,  KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,   S(KC_RBRC), S(KC_BSLS), KC_N,   KC_END,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
+    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_LBRC,  _______,   _______,_______, KC_UP, KC_RGHT, KC_QUOT, \
+    _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,  S(KC_RBRC), S(KC_BSLS), KC_N,   KC_END,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
     _______, _______, _______, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_LEFT,  KC_DOWN, _______, _______ \
   )
   /*
