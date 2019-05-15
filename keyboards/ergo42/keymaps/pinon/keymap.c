9ae958d490108c1b5c649d228dc3d8af41fe2c37
pinon <ymc9.knit.p23@gmail.com>
2019-05-15 21:55:08 +0900

    調整

--- keyboards/ergo42/keymaps/pinon/keymap.c
+++ keyboards/ergo42/keymaps/pinon/keymap.c
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [GAME] = LAYOUT( \
-    _______,  _______,_______,   _______,_______,   _______,   _______,_______,_______,   _______,KC_PSCR, KC_HOME,_______,    KC_DEL, \
-    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_LBRC,  _______,   _______,_______, KC_UP, KC_RGHT, KC_QUOT, \
-    _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,  S(KC_RBRC), S(KC_BSLS), KC_N,   KC_END,    KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
+    _______,  _______,_______,   _______,_______, _______,     _______,_______,_______,_______,KC_PSCR, KC_HOME,_______,    KC_DEL, \
+    _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_LBRC,     _______,_______,_______,_______, KC_UP, KC_RGHT,_______, \
+    _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,    S(KC_RBRC), S(KC_BSLS), KC_END,KC_LEFT,  KC_DOWN,  KC_RGHT, SFT_T(KC_RO), \
     _______, _______, _______, KC_PSCR, KC_ESC, KC_SPC, KC_TAB,     KC_BSPC,    KC_ENT, KC_DELT, KC_LEFT,  KC_DOWN, _______, _______ \
   )
   /*
