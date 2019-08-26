b26410a446659ff664bab240c103ad9bff3c3bf7
pinon <ymc9.knit.p23@gmail.com>
2019-08-26 14:36:47 +0900

    右guiと ~ ,`の追加

--- keyboards/ergo42/keymaps/pinon/keymap.c
+++ keyboards/ergo42/keymaps/pinon/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,KC_T,KC_Y,          KC_T,KC_Y,KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
     LCTL_T(KC_ESC),  KC_A,    KC_S,   KC_D,    KC_F,KC_G,KC_H,          KC_G,KC_H,KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,KC_B,KC_N,          KC_B, KC_N,KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_ENT), \
-    KC_ESC,  KC_ESC, KC_LALT,KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_LEFT, KC_DOWN, KC_DOWN, MO(GAME) \
+    KC_ESC,  KC_ESC, KC_LALT,KC_LGUI, MO(LOWER), MO(LOWER), KC_SPC, KC_ENT, MO(RAISE), MO(RAISE), KC_RGUI, KC_DOWN, KC_DOWN, MO(GAME) \
   ),
 
   /* LOWER
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [LOWER] = LAYOUT( \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______, _______,S(KC_6), S(KC_7), S(KC_8), S(KC_9),    S(KC_0),  S(KC_EQL), \
+    KC_TILD, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______, _______,S(KC_6), S(KC_7), S(KC_8), S(KC_9),    S(KC_0),  S(KC_EQL), \
     _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_LBRC,    KC_RBRC, KC_LEFT, S(KC_MINS), S(KC_EQL), S(KC_LBRC), S(KC_RBRC), KC_BSLS, \
     _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,    _______, KC_F11,   KC_F12, KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
     _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [RAISE] = LAYOUT( \
-    _______, KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  _______,       _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, KC_DEL, \
+    KC_GRV, KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  _______,       _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, KC_DEL, \
     _______, KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, S(KC_LBRC),    S(KC_RBRC), S(KC_7), KC_MINS    , KC_EQL, KC_LBRC,   KC_RBRC, S(KC_BSLS), \
     _______, KC_F6,   KC_F7, KC_F8, KC_F9, KC_F10,_______,    _______, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,    _______ \
